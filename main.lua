dc08c3b13a12a1da8801e47c1e0dcac1c6e6e4cf
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-04 11:23:37 +0900

    Update and rename .gitignore to main.lua

---
+++ main.lua
@@ -0,0 +1,18 @@
+local teleportFunc = queueonteleport or queue_on_teleport
+    if teleportFunc then
+        teleportFunc([[
+            if not game:IsLoaded() then
+                game.Loaded:Wait()
+            end
+            repeat wait() until game.Players.LocalPlayer
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-554, 177, 56)
+wait(0.7)
+for i,v in ipairs(game:GetService("Workspace"):GetDescendants()) do
+            if v.ClassName == "ProximityPrompt" then
+                fireproximityprompt(v)
+            end
+        end
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/auto-get-fan-boxer-gloves/main/main.lua'))()
+]])
+end
+game:GetService("TeleportService"):Teleport(17290438723)
